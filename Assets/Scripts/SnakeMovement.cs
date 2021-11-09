31ca43d4a498617e73af9ae00371c42b34175b2f
Luis Corales <luisaugcorales@gmail.com>
2021-11-09 18:44:27 -0500

    Add snake movement

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class SnakeMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject body;
+
+    [SerializeField] float speed = 3f;
+
+    string currentDirection;
+    string oldDirection;
+
+    Transform headPosition;
+
+    void Awake()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleInput();
     }
+
+    void HandleInput()
+    {
+        oldDirection = currentDirection;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentDirection = "up";
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            currentDirection = "down";
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentDirection = "left";
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentDirection = "right";
+        }
+
+        switch(currentDirection.ToLower())
+        {
+            case "up":
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1), Time.deltaTime * speed);
+            break;
+
+            case "down":
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z - 1), Time.deltaTime * speed);
+            break;
+
+            case "left":
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z), Time.deltaTime * speed);
+            break;
+
+            case "right":
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Time.deltaTime * speed);
+            break;
+
+            default:
+                
+            break;
+        }
+    }
+
 }
